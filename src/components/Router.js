022b1e6ae2b6e0ed6a33d12b62cf8753a8590695
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-08-26 00:50:28 +0100

    Added quicksand font

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,21 +2,21 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Welcome from "../routes/Welcome";
 import Login from "../routes/Login";
-import App from "../routes/App";
 import Register from "../routes/Register";
-import NotFound from "../routes/NotFound";
 import UpdatesByDate from "../routes/UpdatesByDate";
 import UpdatesByTags from "../routes/UpdatesByTags";
+import App from "../routes/App";
+import NotFound from "../routes/NotFound";
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Welcome} />
       <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
       <Route exact path="/app/updatesbydate" component={UpdatesByDate} />
       <Route exact path="/app/updatesbytags" component={UpdatesByTags} />
-      <Route path="/app/:userId" component={App} />
-      <Route exact path="/register" component={Register} />
+      <Route path="/app/:username" component={App} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
