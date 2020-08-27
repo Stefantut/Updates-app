fd4b255a8b36b297ffc11e1defc8fb954565e275
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-08-27 23:59:43 +0100

    Created redirect function

--- src/utils/Context.js
+++ src/utils/Context.js
@@ -1,16 +1,31 @@
 import React, { useState, createContext } from "react";
+import { useHistory } from "react-router-dom";
 
 export const Context = createContext();
 
 // Provider will just provide information to different components
 export const Provider = (props) => {
-  const [isLogged, setIsLogged] = useState(false);
+  const [isLogged, setIsLogged] = useState(true);
+
+  // redirect home
+  const history = useHistory();
+
+  const redirect = (path) => {
+    let newPath = path;
+    history.push(newPath);
+  };
+
+  const logOut = () => {
+    setIsLogged(false);
+    redirect("/");
+  };
 
   // props.children will render all child components, whatever is wrapped between
   return (
     <Context.Provider
       value={{
-        userLoggedIn: [isLogged, setIsLogged],
+        isLogged,
+        logOut,
       }}
     >
       {props.children}
