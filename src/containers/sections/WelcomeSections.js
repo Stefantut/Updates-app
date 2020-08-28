6747892f44811ae260c480a58ae5c4c6f1b9adc8
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-08-28 23:53:47 +0100

    Added Login Button

--- src/containers/sections/WelcomeSections.js
+++ src/containers/sections/WelcomeSections.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Title from "../../components/layouts/Title";
 
+import Button from "../../components/layouts/Button";
+import { ReactComponent as Forward } from "../../images/forward.svg";
+
 const WelcomeSection = () => {
   return (
     <div className="welcome-section">
       <Title title={"Welcome"} />
+      <Button path={"/login"} image={<Forward />} text={"Login"} />
     </div>
   );
 };
