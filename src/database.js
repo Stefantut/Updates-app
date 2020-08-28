2a353b896f0c1eaf3779400b224b18a42a95ac88
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-08-29 00:48:52 +0100

    Added fake database

---
+++ src/database.js
@@ -0,0 +1,81 @@
+// fake database
+export const users = [
+  { name: "stefan", email: "stefan@dev.com", password: "aaaaa" },
+  { name: "stefan1", email: "stefan@dev.com", password: "stefan1" },
+  { name: "stefan2", email: "stefan2@dev.com", password: "stefan2" },
+];
+
+export const updates = [
+  {
+    title: "1 Lorem ipsum",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    tags: ["javascript", "react"],
+    user: "stefan",
+    timestamp: "2020-12-14",
+  },
+  {
+    title: "2 Lorem ipsum ",
+    text:
+      "2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    tags: ["javascript", "vue"],
+    user: "stefan1",
+    timestamp: "2018-03-27",
+  },
+  {
+    title: "3 Lorem ipsum ",
+    text:
+      "3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    tags: ["react", "vue"],
+    user: "stefan2",
+    timestamp: "2020-05-26",
+  },
+  {
+    title: "4 Lorem ipsum ",
+    text:
+      "4 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    tags: ["javascript"],
+    user: "stefan1",
+    timestamp: "2019-01-15",
+  },
+  {
+    title: "5 Lorem ipsum ",
+    text:
+      "5 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    tags: ["javascript", "vue"],
+    user: "stefan2",
+    timestamp: "2018-03-26",
+  },
+  {
+    title: "6 Lorem ipsum ",
+    text:
+      "6 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    tags: ["vue"],
+    user: "stefan1",
+    timestamp: "2016-05-26",
+  },
+  {
+    title: "7 Lorem ipsum ",
+    text:
+      "7 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    tags: ["react"],
+    user: "stefan1",
+    timestamp: "2020-08-06",
+  },
+  {
+    title: "8 Lorem ipsum ",
+    text:
+      "8 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    tags: ["javascript"],
+    user: "stefan1",
+    timestamp: "2019-04-20",
+  },
+  {
+    title: "9 Lorem ipsum ",
+    text:
+      "9 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    tags: ["vue"],
+    user: "stefan1",
+    timestamp: "2018-01-12",
+  },
+];
