60a498637ded4abfdb1ad22bfd64e0a0f17fa165
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-08-28 01:44:28 +0100

    Built layout Welcome Section

---
+++ src/containers/sections/WelcomeSections.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "../../components/layouts/Title";
+
+const WelcomeSection = () => {
+  return (
+    <div className="welcome-section">
+      <Title title={"Welcome"} />
+    </div>
+  );
+};
+
+export default WelcomeSection;
--- src/routes/Welcome.js
+++ src/routes/Welcome.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import SectionWrap from "../components/layouts/SectionWrap";
 import BigTitle from "../components/BigTitle";
+import WelcomeSection from "../containers/sections/WelcomeSections";
 
 function Welcome() {
   return (
     <SectionWrap
       className="welcome content"
       leftSection={<BigTitle />}
-      rightSection={<BigTitle />}
+      rightSection={<WelcomeSection />}
     ></SectionWrap>
   );
 }
