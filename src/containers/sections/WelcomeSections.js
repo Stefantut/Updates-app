db6a375b12f24ae7a8ed75053af5df9d804ed068
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-08-30 00:36:18 +0100

    Conditional rendering of buttons

--- src/containers/sections/WelcomeSections.js
+++ src/containers/sections/WelcomeSections.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import Title from "../../components/layouts/Title";
+
+import { Context } from "../../utils/Context";
 
 import Button from "../../components/layouts/Button";
 import TextLine from "../../components/layouts/TextLine";
 
 const WelcomeSection = () => {
+  const { isLogged } = useContext(Context);
   return (
     <div className="welcome-section fifty-section">
       <Title title={"Welcome"} />
-      <div className="wrap-content">
-        <Button path={"/login"} text={"Login"} />
-        <Button path={"/register"} text={"Register"} />
-      </div>
+      {!isLogged && (
+        <div className="wrap-content">
+          <Button path={"/login"} text={"Login"} />
+          <Button path={"/register"} text={"Register"} />
+        </div>
+      )}
       <TextLine text={"All updates in one place."} />
     </div>
   );
