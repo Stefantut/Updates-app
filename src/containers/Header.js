ef40c191b28760dd1c6f141b797e1ab6d93e0070
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-08-30 00:48:12 +0100

    Header displays name of the user and updates links

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,20 +1,30 @@
 import React, { useContext } from "react";
 
 import { Context } from "../utils/Context";
+import { Link } from "react-router-dom";
 import Logo from "../components/Logo";
 
 const Header = () => {
-  const { isLogged, logOut } = useContext(Context);
+  const { isLogged, logOut, loggedUser } = useContext(Context);
 
   return (
     <header className="content">
-      <Logo />
+      <Link to="/">
+        <Logo />
+      </Link>
       {isLogged && (
-        <div className="welcome-msg">
-          Welcome Username,
-          <span className="button" onClick={logOut}>
-            Logout
-          </span>
+        <div className="top-wrap">
+          <div className="welcome-msg">
+            Welcome {loggedUser},
+            <span className="button" onClick={logOut}>
+              Logout
+            </span>
+          </div>
+          <div className="updates-wrap">
+            View all updates:
+            <Link to="/app/updatesbytags">by tags</Link>
+            <Link to="app/updatesbydate">by date</Link>
+          </div>
         </div>
       )}
     </header>
