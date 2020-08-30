9c4bdb1770180348bb548bf69d4536ec194250bd
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-08-31 00:13:32 +0100

    Created Register layout

---
+++ src/containers/sections/RegisterSection.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Title from "../../components/layouts/Title";
+
+const RegisterSection = () => {
+  return (
+    <div className="register-section fifty-section">
+      <Title title={"Register"} />
+    </div>
+  );
+};
+
+export default RegisterSection;
--- src/routes/Register.js
+++ src/routes/Register.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+import SectionWrap from "../components/layouts/SectionWrap";
+import BigTitle from "../components/BigTitle";
+import RegisterSection from "../containers/sections/RegisterSection";
+
 const Register = () => {
-  return <div className="Register">register</div>;
+  return (
+    <SectionWrap
+      className="Welcome"
+      leftSection={<BigTitle />}
+      rightSection={<RegisterSection />}
+    ></SectionWrap>
+  );
 };
 
 export default Register;
